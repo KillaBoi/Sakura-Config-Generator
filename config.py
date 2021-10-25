2723520234b8af6efbf4ffcba4848fccb3b10d90
Killa <46409262+KillaBoi@users.noreply.github.com>
2021-10-25 04:30:28 +0100

    galil

--- config.py
+++ config.py
@@ -1784,3 +1784,133 @@ while True:
             else:
                 print("[FAMAS] Invalid Option")
                 time.sleep(2)
+    
+    elif weapon_id == 14: #galil
+        while True:
+            cls()
+            print("[GALIL] Options")
+            print(Options)
+            option_select = int(input("Option: "))
+            if option_select == 1:
+                aim_key = int(input("[GALIL] Aim Key: "))
+                galil[0]['aim_key'] = aim_key
+                print("[GALIL] Aim Key set to: " + str(aim_key))
+                time.sleep(2)
+            elif option_select == 2:
+                aim_rotation = int(input("[GALIL] Aim Rotation: "))
+                galil[0]['aim_rotation'] = aim_rotation
+                print("[GALIL] Aim Rotation set to: " + str(aim_rotation))
+                time.sleep(2)
+
+            elif option_select == 3:
+                aim_stick = int(input("[GALIL] Sticky Aim: "))
+                galil[0]['aim_stick'] = aim_stick
+                print("[GALIL] Sticky Aim set to: " + str(aim_stick))
+                time.sleep(2)
+
+            elif option_select == 4:
+                print("unknown value, not changing")
+
+            elif option_select == 5:
+                aim_fov = float(input("[GALIL] Aim FOV: "))
+                galil[0]['aim_fov'] = aim_fov
+                print("[GALIL] Aim FOV set to: " + str(aim_fov))
+                time.sleep(2)
+            
+            elif option_select == 6:
+                bone_selection = int(input("[GALIL] Bone Selection: "))
+                galil[0]['bone_selection'] = bone_selection
+                print("[GALIL] Bone Selection set to: " + str(bone_selection))
+                time.sleep(2)
+            
+            elif option_select == 7:
+                bone_3 = int(input("[GALIL] Bone 3: "))
+                galil[0]['bone_3'] = bone_3
+                print("[GALIL] Bone 3 set to: " + str(bone_3))
+                time.sleep(2)
+            
+            elif option_select == 8:
+                bone_2 = int(input("[GALIL] Bone 2: "))
+                galil[0]['bone_2'] = bone_2
+                print("[GALIL] Bone 2 set to: " + str(bone_2))
+                time.sleep(2)
+            
+            elif option_select == 9:
+                bone_1 = int(input("[GALIL] Bone 1: "))
+                galil[0]['bone_1'] = bone_1
+                print("[GALIL] Bone 1 set to: " + str(bone_1))
+                time.sleep(2)
+            
+            elif option_select == 10:
+                bone_0 = int(input("[GALIL] Bone 0: "))
+                galil[0]['bone_0'] = bone_0
+                print("[GALIL] Bone 0 set to: " + str(bone_0))
+                time.sleep(2)
+            
+            elif option_select == 11:
+                smooth = float(input("[GALIL] Smooth: "))
+                galil[0]['smooth'] = smooth
+                print("[GALIL] Smooth set to: " + str(smooth))
+                time.sleep(2)
+            
+            elif option_select == 12:
+                aim_delay = int(input("[GALIL] Aim Delay: "))
+                galil[0]['aim_delay'] = aim_delay
+                print("[GALIL] Aim Delay set to: " + str(aim_delay))
+                time.sleep(2)
+            
+            elif option_select == 13:
+                aim_time = int(input("[GALIL] Aim Time: "))
+                galil[0]['aim_time'] = aim_time
+                print("[GALIL] Aim Time set to: " + str(aim_time))
+                time.sleep(2)
+            
+            elif option_select == 14:
+                aim_thru_walls = int(input("[GALIL] Aim Through Walls: "))
+                galil[0]['aim_thru_walls'] = aim_thru_walls
+                print("[GALIL] Aim Through Walls set to: " + str(aim_thru_walls))
+                time.sleep(2)
+            
+            elif option_select == 15:
+                trigger_key = int(input("[GALIL] Trigger Key: "))
+                galil[0]['trigger_key'] = trigger_key
+                print("[GALIL] Trigger Key set to: " + str(trigger_key))
+                time.sleep(2)
+            
+            elif option_select == 16:
+                trigger_delay = int(input("[GALIL] Trigger Delay: "))
+                galil[0]['trigger_delay'] = trigger_delay
+                print("[GALIL] Trigger Delay set to: " + str(trigger_delay))
+                time.sleep(2)
+            
+            elif option_select == 17:
+                trigger_pause = int(input("[GALIL] Trigger Pause: "))
+                galil[0]['trigger_pause'] = trigger_pause
+                print("[GALIL] Trigger Pause set to: " + str(trigger_pause))
+                time.sleep(2)
+            
+            elif option_select == 18:
+                trigger_after_shot = int(input("[GALIL] Trigger After Shot: "))
+                galil[0]['trigger_after_shot'] = trigger_after_shot
+                print("[GALIL] Trigger After Shot set to: " + str(trigger_after_shot))
+                time.sleep(2)
+            
+            elif option_select == 19:
+                horizontal_rcs = float(input("[GALIL] Horizontal RCS: "))
+                galil[0]['horizontal_rcs'] = horizontal_rcs
+                print("[GALIL] Horizontal RCS set to: " + str(horizontal_rcs))
+                time.sleep(2)
+            
+            elif option_select == 20:
+
+                vertical_rcs = float(input("[GALIL] Vertical RCS: "))
+                galil[0]['vertical_rcs'] = vertical_rcs
+                print("[GALIL] Vertical RCS set to: " + str(vertical_rcs))
+                time.sleep(2)
+
+            elif option_select == 0:
+                break
+        
+            else:
+                print("[GALIL] Invalid Option")
+                time.sleep(2)
